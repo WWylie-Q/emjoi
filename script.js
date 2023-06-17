346e84ccfd7d0e16d80ee11aab13078d4d95382a
Wendall Wylie <wendallwylie81@gmail.com>
2023-06-17 16:35:29 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+const buttons = document.querySelectorAll(".button");
+const emoji = document.getElementById("emoji");
+
+for (const button of buttons) {
+  button.addEventListener("click", () => {
+    button.classList.toggle("button--active");
+    console.log("emoji html ", emoji.innerHTML)
+    emoji.innerHTML === "🖕" ? emoji.innerHTML= "😄" : emoji.innerHTML = "🖕";
+  });
+}
